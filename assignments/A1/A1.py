2b9dd3bb9bafa8245056e795715aaeef3001aa4d
Cplan082 <46877705+Cplan082@users.noreply.github.com>
2022-02-12 12:02:29 -0500

    All three input types are functioning as intended in A1 Q2

--- assignments/A1/A1.py
+++ assignments/A1/A1.py
@@ -12,6 +12,11 @@ from numpy.linalg import inv
 from scipy.signal import unit_impulse 
 import matplotlib.pyplot as mpl
 
+# testing vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+# df = pd.read_csv('noise_set.csv')
+# noise = df['0']
+# testing ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
 sample_depth = 400
 
 a1 = 1.3
@@ -33,6 +38,7 @@ u_t = np.stack([u_t1, u_t2, u_t3]) # impulse = u_t[0], step = u_t[1]
 
 sigma = 0.65
 y0 = np.random.normal(0, sigma)
+# y0 = np.array(noise[0]) # for testing **************************************
 # y = [[y0],[y0]] # first elements in y vector
 y = [[y0] for i in range(len(u_t))]
 
@@ -43,7 +49,7 @@ theta_hat = [[theta_hat0] for i in range(len(u_t))]
 #%%
 
 for j in range(len(u_t)):
-    print(j)
+    p =100*np.identity(4) # starting P matrix
     for i in range(1,sample_depth):
         if (i == 1): # accounts for the lack of t-2 data on first iteration
             phi = np.array([-y[j][i-1], 0, u_t[j][i-1], 0])
@@ -52,6 +58,7 @@ for j in range(len(u_t)):
             
         phi = np.asarray(phi).reshape(-1,1) # changes phi's dimensions from (4,) to [4,1] enabling transpose operations
         y[j].append(np.reshape(phi.T@theta0 + np.random.normal(0, sigma), ()))
+        # y[j].append(np.reshape(phi.T@theta0 + np.array(noise[i]), ()))
         p = inv(inv(p) + phi*phi.T)
         k = p@phi
         theta_hat[j].append(theta_hat[j][i-1] + k*(y[j][i] - phi.T@theta_hat[j][i-1]))
@@ -61,8 +68,12 @@ df_lst = [pd.DataFrame(np.asarray(theta_hat[i]).reshape(-1,4,),
 
 #%%
 mpl.plot(t, df_lst[2]['a1'])
+mpl.plot(t, [a1 for i in range(sample_depth)])
+
 mpl.plot(t, df_lst[2]['a2'])
-# mpl.plot(t, df_lst[2]['b0'])
-# mpl.plot(t, df_lst[2]['b1'])
+
+mpl.plot(t, df_lst[2]['b0'])
+
+mpl.plot(t, df_lst[2]['b1'])
         
 
