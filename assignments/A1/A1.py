dad6cac7e8353100db8ae084d8eaa1de3daf1229
Cplan082 <46877705+Cplan082@users.noreply.github.com>
2022-02-13 18:54:18 -0500

    Completed explanations in jupyter notbook of A1 up to Q2 part 1

--- assignments/A1/A1.py
+++ assignments/A1/A1.py
@@ -5,12 +5,14 @@ Created on Thu Feb 10 21:05:47 2022
 @author: clive
 """
 import numpy as np
+from numpy import sqrt
+from numpy.linalg import inv, matrix_rank
 import pandas as pd
 from math import cos, sin, pi
 import scipy as spy
-from numpy.linalg import inv
 from scipy.signal import unit_impulse 
 import matplotlib.pyplot as mpl
+import sympy as sym
 
 # testing vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 # df = pd.read_csv('noise_set.csv')
@@ -48,6 +50,9 @@ theta_hat = [[theta_hat0] for i in range(len(u_t))]
 
 #%%
 
+err_arr = []
+phi_arr = []
+
 for j in range(len(u_t)):
     p =100*np.identity(4) # starting P matrix
     for i in range(1,sample_depth):
@@ -56,16 +61,42 @@ for j in range(len(u_t)):
         else:
             phi = np.array([-y[j][i-1], -y[j][i-2], u_t[j][i-1], u_t[j][i-2]])
             
-        phi = np.asarray(phi).reshape(-1,1) # changes phi's dimensions from (4,) to [4,1] enabling transpose operations
+        phi = np.asarray(phi).reshape(-1,1) # changes phi's dimensions from (4,) to [4,1] enabling transpose operations            
         y[j].append(np.reshape(phi.T@theta0 + np.random.normal(0, sigma), ()))
         # y[j].append(np.reshape(phi.T@theta0 + np.array(noise[i]), ()))
         p = inv(inv(p) + phi*phi.T)
         k = p@phi
+        if (j == 1):
+            err_arr.append(np.reshape(y[j][i].tolist() - (phi.T@theta_hat[j][i-1]), ()))
+            phi_arr.append(np.asarray(phi).reshape(-1,))
+            
         theta_hat[j].append(theta_hat[j][i-1] + k*(y[j][i] - phi.T@theta_hat[j][i-1]))
 
 df_lst = [pd.DataFrame(np.asarray(theta_hat[i]).reshape(-1,4,), 
                    columns=['a1', 'a2', 'b0', 'b1']) for i in range(len(u_t))]
 
+#%%
+phi_cap = np.stack(phi_arr)
+print(matrix_rank(phi_cap.T@phi_cap))
+print(sym.Matrix(phi_cap.T@phi_cap).rref())
+
+
+#%%
+
+# err = np.reshape(np.array(err_arr), (-1,1))
+# x = err.T@err/2
+# sigma = np.sqrt(2*x/(3000-4))
+# print(sigma)
+
+#%%
+# loss_b0 = np.array(sigma - df_lst[2]['b0']).reshape(-1,1)
+# sigma_hat_b0 = sqrt(2*loss_b0.T@loss_b0/(sample_depth - 4))
+
+# loss_b1 = np.array(sigma - df_lst[2]['b1']).reshape(-1,1)
+# sigma_hat_b0 = sqrt(2*loss_b1.T@loss_b1/(sample_depth - 4))
+
+# print(sigma_hat_b0)
+# print(sigma_hat_b1)
 #%%
 # mpl.plot(t, df_lst[2]['a1'])
 
@@ -97,15 +128,15 @@ df_lst = [pd.DataFrame(np.asarray(theta_hat[i]).reshape(-1,4,),
 # ax4.legend()
 # ax4.set_title('b1')
 
-import matplotlib.pyplot as mpl
-import seaborn as sns
-line_width = 0.8
-graph = sns.lineplot(data=df_lst[2], dashes=False)
-# graph.despine(left=True)
-graph.axhline(y=a1, color='black', linestyle='--', linewidth=line_width, label='a1')
-graph.axhline(y=a2, color='black', linestyle='--', linewidth=line_width, label='a2')
-graph.axhline(y=b0, color='black', linestyle='--', linewidth=line_width, label='b0')
-graph.axhline(y=b1, color='black', linestyle='--', linewidth=line_width, label='b1')
+# import matplotlib.pyplot as mpl
+# import seaborn as sns
+# line_width = 0.8
+# graph = sns.lineplot(data=df_lst[2], dashes=False)
+# # graph.despine(left=True)
+# graph.axhline(y=a1, color='black', linestyle='--', linewidth=line_width, label='a1')
+# graph.axhline(y=a2, color='black', linestyle='--', linewidth=line_width, label='a2')
+# graph.axhline(y=b0, color='black', linestyle='--', linewidth=line_width, label='b0')
+# graph.axhline(y=b1, color='black', linestyle='--', linewidth=line_width, label='b1')
 
-mpl.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0, labels=df_lst[2].columns)
+# mpl.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0, labels=df_lst[2].columns)
 
